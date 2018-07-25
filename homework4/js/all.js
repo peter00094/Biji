f48dca2b9a9cbeacfc0dfa7bc9c87b624a81047c
peter00094 <peter00094@gmail.com>
2018-07-25 15:17:38 +0800

    init

---
+++ homework4/js/all.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	
+$('.titleBlock').click(function(event) 
+{
+});
+
+$('.add-btn:last').click(function(event) {
+	$('.biBlock.add.hide').show();
+	$('.add-btn:last').hide();
+});
+
+$('.add-btn:last').prev().click(function(event) {
+	$('.biBlock.hideTitle').show();
+	$('.biBlock:last').prev().prepend('<div class="infoContent" id="aa"><select class="wid"  name="city" id="city_selector";><option value selected="true" disabled="disabled">開車</option></select>		<div class="infoContent wiid" ><input type="text" name="title" placeholder="詳細說明"></div></div>')
+});
+
+});
+
